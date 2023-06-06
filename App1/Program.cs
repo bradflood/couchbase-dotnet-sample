25d4cd0447c759fe372a09b21fcc119f521f5963
Brad Flood <bflood@keyholesoftware.com>
2023-06-06 12:44:21 -0500

    remove ignore cert mismatch

--- App1/Program.cs
+++ App1/Program.cs
@@ -20,7 +20,7 @@ class CloudExample
 
         var clusterOptions = new ClusterOptions{
             ForceIpAsTargetHost = true,
-            KvIgnoreRemoteCertificateNameMismatch = true // development only. do not include in any production configuration
+            // KvIgnoreRemoteCertificateNameMismatch = true // development only. do not include in any production configuration
         }
         .WithConnectionString(connection_string)
         .WithCredentials(username: userid, password: password)
